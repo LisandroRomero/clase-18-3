9a537e7d416656420accfa05d95e501b83523e07
LisandroRomero <lisanromero18@gmail.com>
2025-03-25 07:49:58 -0300

    clase 16/3 subida

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiante estudiante1 = new Estudiante("Lisandro","Romero", "Programacion",7.5);
+
+
+        estudiante1.mostrarinfo();
+        estudiante1.bajarCalificacion(4.5);
+        estudiante1.mostrarinfo();
+        estudiante1.subirCalificacion(6.0);
+        estudiante1.mostrarinfo();
+
+    }
+}
